522c6f9697b97ec1aff31f86a666e6cfe6f7b4ad
bschwartz10 <bschwartz92@gmail.com>
2017-05-17 11:18:11 -0600

    delete comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,6 @@ class SearchController < ApplicationController
 
   def index
     @fuel_stations = NrlApi.find_by_zipcode(params[:q])
-    # zipcode = params[:q]
-    # api_key = ENV["nrel_api_key"]
-    # response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?location=#{zipcode}&limit=10&fuel_type=ELEC,LPG&api_key=#{api_key}&format=JSON"
-    # body = JSON.parse(response.body)
-    # @fuel_stations = body["fuel_stations"]
   end
 
 end
